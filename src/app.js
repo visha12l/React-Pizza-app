7f68255adc8015bdcebe17fb7cd27276a0000378
visha12l <vsvshinde15@gmail.com>
2017-12-09 11:53:32 +0530

    adds pizzalisting component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import PizzaListing from './javascript/components/pizzaListing.js';
+import './css/style.css';
 
 ReactDom.render((
    <Router>
--- src/javascript/components/pizzaListing.js
+++ src/javascript/components/pizzaListing.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import pizza from '../../static/result.json';
+import pizza from '../../static/pizzas.json';
+import underscore from 'underscore';
 
-const PizzaListing = () =>  {
-    return (
-        <div className="container">
-            
-        </div>
-    );
+export default class PizzaListing extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            pizzaDetail: pizza.pizzaList
+        };
+        this.orderPizza = this.orderPizza.bind(this);
+    }
+
+    orderPizza(pizzaName, key) {
+        
+    }
+
+    render() {
+        return (
+            <div className="container clearfix">
+                <ul className="row clearfix pizzaListing">
+                    {underscore.map(this.state.pizzaDetail, (item, key) => {
+                        return (
+                            <li key={key} className="col-md-4">
+                                <div className="text-center">
+                                    <img className="img-responsive" src={item.imageUrl} />
+                                    <h3>{item.name}</h3>
+                                    <p>{item.description}</p>
+                                    <a href="#" onClick={this.orderPizza.bind(this, item, key)}>ORDER NOW</a>
+                                </div>
+                            </li>
+                          )  
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
 };
-export default PizzaListing;
