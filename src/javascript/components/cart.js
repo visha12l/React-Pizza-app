8d702d979632da26933e1cb52b5e26a4c011b888
visha12l <vsvshinde15@gmail.com>
2018-05-25 15:06:18 +0530

    code refactoring using eslint

--- src/javascript/components/cart.js
+++ src/javascript/components/cart.js
@@ -1,122 +1,130 @@
 import React from 'react';
 import underscore from 'underscore';
+import PropTypes from 'prop-types';
 import couponCodes from '../../static/coupon.json';
 
 export default class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      couponData: couponCodes.coupon,
+      couponDiscount: 0,
+    };
+    this.couponInput = React.createRef();
+    this.floatingLabel = React.createRef();
+    this.removeItem = this.removeItem.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.resetCouponCode = this.resetCouponCode.bind(this);
+    this.applyCoupon = this.applyCoupon.bind(this);
+    this.placeOrder = this.placeOrder.bind(this);
+  }
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            value: '',
-            couponData: couponCodes.coupon,
-            couponDiscount: 0
+  removeItem(key) {
+    this.resetCouponCode();
+    this.props.removeItem(key);
+  }
+
+  applyCoupon() {
+    const userInput = this.couponInput.value.toUpperCase();
+    if (userInput) {
+      const result = underscore.filter(this.state.couponData, (item) => {
+        if (userInput === item.name) {
+          this.setState({
+            couponDiscount: item.discount,
+          });
+          return item;
         }
-        this.removeItem = this.removeItem.bind(this);
-        this.handleInputBlur = this.handleInputBlur.bind(this);
-        this.handleInputFocus = this.handleInputFocus.bind(this);
-        this.resetCouponCode = this.resetCouponCode.bind(this);
-        this.applyCoupon = this.applyCoupon.bind(this);
-        this.placeOrder = this.placeOrder.bind(this);
-    }
-
-    removeItem(key) {
+      });
+      if (underscore.isEmpty(result)) {
+        alert('invalid code !!!');
         this.resetCouponCode();
-        this.props.removeItem(key);
+      }
+    } else {
+      alert('empty code !!!');
+      this.resetCouponCode();
     }
+    this.couponInput.value = '';
+  }
 
-    applyCoupon() {
-        let userInput = this.refs.couponInput.value.toUpperCase();
-        if (userInput) {
-            let result = underscore.filter(this.state.couponData, (item) => {
-                 if(userInput === item.name) {
-                     this.setState({
-                        couponDiscount: item.discount
-                    });
-                    return item;
-                } 
-            });
-            if(underscore.isEmpty(result)) {
-                alert('invalid code !!!');
-                this.resetCouponCode();
-            }
-        } else {
-            alert('empty code !!!');
-            this.resetCouponCode();
-        } 
-        this.refs.couponInput.value = "";
-    }
+  placeOrder(total) {
+    this.props.placeOrder(total);
+  }
 
-    placeOrder(total) {
-        this.props.placeOrder(total);
-    }
+  resetCouponCode() {
+    this.setState({
+      couponDiscount: 0,
+    });
+  }
 
-    resetCouponCode() {
-        this.setState({
-            couponDiscount: 0
-        });
-    }
+  handleInputFocus() {
+    this.floatingLabel.classList.remove('hide');
+  }
 
-    handleInputFocus(refName) {
-        this.refs[refName].classList.remove("hide"); 
-    }
+  handleInputBlur() {
+    this.floatingLabel.classList.add('hide');
+  }
 
-    handleInputBlur(refName) {
-        this.refs[refName].classList.add("hide");
-    }
-
-    render() {
-        let {cartArray} = this.props;
-        let total = cartArray.reduce((a, b) => {
-            return a + b.finalPrice;
-        }, 0);
-        total = total - total * this.state.couponDiscount / 100;
-        return (
-            <div className="userCart">
-                <ul className="list-group">
-                    {cartArray.length > 0 && 
-                        <li className="clearfix list-group-item list-group-item-dark">
-                            <h3 className="pull-left name">Name</h3>
-                            <h3 className="pull-right action">Action</h3>
-                            <h3 className="pull-right price">Price</h3>
-                        </li>
-                    }
-                    {cartArray && 
-                        underscore.map(cartArray, (item, key) => {
-                            return (
-                                <li className="list-group-item clearfix list-group-item-success" key={key}>
-                                    <h4 className="pull-left name">{key + 1}){item.name}</h4>
-                                    <button 
-                                        className="btn action removeItemBtn pull-right btn-danger"
-                                        onClick={this.removeItem.bind(this, key)}
-                                    >X</button>
-                                    <span className="pull-right price">{item.finalPrice}RS</span>
-                                </li>
-                            )
-                        })
-                    } 
-                </ul>
-                {cartArray.length > 0 &&
-                    <div className="totalPriceWrap text-center">
-                        {this.state.couponDiscount ? <p className="couponMessage">Congratulations Coupon Applied !!!</p> : null} 
-                        <div className="clearfix">
-                            <div className="pull-left couponWrap">
-                                <input 
-                                    ref='couponInput'
-                                    type="text"
-                                    className="couponInput"
-                                    placeholder="Enter Code"
-                                    onFocus={this.handleInputFocus.bind(this, 'floatingLabel')}
-                                    onBlur={this.handleInputBlur.bind(this, 'floatingLabel')}
-                                />
-                                <span className="floatingLabel hide" ref="floatingLabel">Enter Code</span>
-                                <button className="btn btn-info" onClick={this.applyCoupon}>Apply Coupon</button>
-                            </div>
-                            <span className="pull-right">TOTAL => {total} RS</span>
-                        </div>
-                        <button className="btn redBtn" onClick={this.placeOrder.bind(this, total)}>Place Your Order</button>
-                    </div>
-                }
+  render() {
+    const { cartArray } = this.props;
+    let total = cartArray.reduce((a, b) => a + b.finalPrice, 0);
+    total -= total * (this.state.couponDiscount / 100);
+    return (
+      <div className="userCart">
+        <ul className="list-group">
+          {cartArray.length > 0 &&
+            <li className="clearfix list-group-item list-group-item-dark">
+              <h3 className="pull-left name">Name</h3>
+              <h3 className="pull-right action">Action</h3>
+              <h3 className="pull-right price">Price</h3>
+            </li>
+          }
+          {cartArray &&
+            underscore.map(cartArray, (item, key) => (
+              <li className="list-group-item clearfix list-group-item-success" key={key}>
+                <h4 className="pull-left name">{key + 1}){item.name}</h4>
+                <button
+                  className="btn action removeItemBtn pull-right btn-danger"
+                  onClick={this.removeItem.bind(this, key)}
+                >X
+                </button>
+                <span className="pull-right price">{item.finalPrice}RS</span>
+              </li>
+            ))
+          }
+        </ul>
+        {cartArray.length > 0 &&
+        <div className="totalPriceWrap text-center">
+          {this.state.couponDiscount ? <p className="couponMessage">Congratulations Coupon Applied !!!</p> : null}
+          <div className="clearfix">
+            <div className="pull-left couponWrap">
+              <input
+                ref={this.couponInput}
+                type="text"
+                className="couponInput"
+                placeholder="Enter Code"
+                onFocus={this.handleInputFocus}
+                onBlur={this.handleInputBlur}
+              />
+              <span className="floatingLabel hide" ref={this.floatingLabel}>Enter Code</span>
+              <button className="btn btn-info" onClick={this.applyCoupon}>Apply Coupon</button>
             </div>
-        );
-    }
+            <span className="pull-right">TOTAL => {total} RS</span>
+          </div>
+          <button className="btn redBtn" onClick={this.placeOrder.bind(this, total)}>Place Your Order</button>
+        </div>
+        }
+      </div>
+    );
+  }
+}
+
+Cart.defaultProps = {
+  placeOrder: undefined,
+  cartArray: [],
+};
+
+Cart.propTypes = {
+  placeOrder: PropTypes.func,
+  cartArray: PropTypes.arrayOf,
 };
--- src/javascript/components/shared/successPopup.js
+++ src/javascript/components/shared/successPopup.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Cart extends React.Component {
+export default class SuccessPopup extends React.Component {
+  constructor() {
+    super();
+    this.closePopup = this.closePopup.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.closePopup = this.closePopup.bind(this);
-    }
+  closePopup() {
+    this.props.closePopup();
+  }
 
-    closePopup() {
-        this.props.closePopup();
-    }
-
-    render() {
-        return (
-            <div className="successPopupWrap">
-                <div className="content">
-                    <p>Thank you for placing the order</p>
-                    <h3>your order will arrive in 30 minutes</h3>
-                    <button className="btn redBtn" onClick={this.closePopup}>OK</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="successPopupWrap">
+        <div className="content">
+          <p>Thank you for placing the order</p>
+          <h3>your order will arrive in 30 minutes</h3>
+          <button className="btn redBtn" onClick={this.closePopup}>OK</button>
+        </div>
+      </div>
+    );
+  }
 }
+
+SuccessPopup.defaultProps = {
+  closePopup: undefined,
+};
+
+SuccessPopup.propTypes = {
+  closePopup: PropTypes.func,
+};
